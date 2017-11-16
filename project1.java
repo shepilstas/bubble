a9d722810b4c18b2f04a31dbcc747c0d19a8fec7
shepilstas <33087144+shepilstas@users.noreply.github.com>
2017-11-16 19:20:47 +0200

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,13 @@
+public class project1 {
+    public static void main (String args []) {
+        System.out.println("      /\\");
+        System.out.println("     /  \\");
+        System.out.println("    / || \\");
+        System.out.println("   /  ||  \\");
+        System.out.println("  /   ||   \\");
+        System.out.println(" /    ||    \\");
+        System.out.println("/     ||     \\");
+        System.out.println("      ||       ");
+        System.out.println("_______________");
+    }
+}
